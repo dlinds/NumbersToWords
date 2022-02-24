7afd5e152e7fcc68cf69ecbe6b27aa7a30fab28c
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-02-24 13:19:05 -0800

    create basic file structure
    Co-authored-by: Eric Crudup <Cruduper@users.noreply.github.com>

---
+++ NumbersToWords.Tests/Models.Tests/NumbersTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Numbers.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Numbers.Tests
+{
+  [TestClass]
+  public class NumbersTests
+  {
+
+  }
+}
---
+++ NumbersToWords/Models/Numbers.cs
@@ -0,0 +1,6 @@
+using System;
+
+namespace Numbers.Models
+{
+
+}
